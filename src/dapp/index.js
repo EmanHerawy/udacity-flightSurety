c277b5822d0add050851625f5b59b800d342b847
emanherawy <eman.herawy@gmail.com>
2020-05-01 03:31:13 +0200

    enable metamask detection

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -217,21 +217,22 @@ console.log(this.account,'this.account');
 window.App = App;
 
 window.addEventListener("load", function () {
-    // if (window.ethereum) {
-    //     // use MetaMask's provider
-    //     App.web3 = new Web3(window.ethereum);
-    //     window.ethereum.enable(); // get permission to access accounts
-    // } else {
-    //     console.warn(
-    //         "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    //     );
-    //     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    //     App.web3 = new Web3(
-    //         new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    //     );
-    // }
-    App.web3 = new Web3(
-        new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
+    if (window.ethereum) {
+        // use MetaMask's provider
+        App.web3 = new Web3(window.ethereum);
+        window.ethereum.enable(); // get permission to access accounts
+    } else {
+        console.warn(
+            "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+        );
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        App.web3 = new Web3(
+            new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+        );
+    }
+    // disable metamsk
+    // App.web3 = new Web3(
+    //     new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+    // );
     App.start();
 });
